4d232f007257172da8e5fc19d30a15e8f6e0891c
WebDevCourserOnGitHub <web.dev.course.vue.usa@gmail.com>
2024-02-21 02:21:11 -0800

    next

--- js/app.js
+++ js/app.js
@@ -3,35 +3,49 @@ const select = document.querySelector('select');
 const allLang = ['en', 'es', 'ru', 'ua'];
 
 document.querySelectorAll('.left-panel img')
-.forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
-
+  .forEach(item => item.onmouseenter = (event) => maxImg.src = event.target.src);
 select.addEventListener('change', changeURLLanguage);
-
 // перенаправить на url с указанием языка
 function changeURLLanguage() {
-    let lang = select.value;
-    location.href = window.location.pathname + '#' + lang;
-    location.reload();
+  let lang = select.value;
+  location.href = window.location.pathname + '#' + lang;
+  location.reload();
 }
-
 function changeLanguage() {
-    let hash = window.location.hash;
-    hash = hash.substr(1);
-    console.log(hash);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + '#es';
-        location.reload();
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+  select.value = hash;
+  document.querySelector('title').innerHTML = langArr['title'][hash];
+  // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
+  for (let key in langArr) {
+    let elem = document.querySelector('.lng-' + key);
+    if (elem) {
+      elem.innerHTML = langArr[key][hash];
     }
-    select.value = hash;
-    document.querySelector('title').innerHTML = langArr['unit'][hash];
-    // document.querySelector('.lng-chip').innerHTML = langArr['chip'][hash];
-    for (let key in langArr) {
-        let elem = document.querySelector('.lng-' + key);
-        if (elem) {
-            elem.innerHTML = langArr[key][hash];
-        }
-
-    }
+  }
 }
 
 changeLanguage();
+
+// document.getElementById('lang-select').addEventListener('change', setLanguageImage);
+
+// function setLanguageImage() {
+//   const selectElement = document.getElementById('lang-select');  // TODO: id for select
+//   const selectedLanguage = selectElement.value;
+//   let imagePath;
+//   switch (selectedLanguage) {
+//     case 'en': imagePath = 'assets/images/qrCodes/AnaheimQRcodeEN.svg'; break;
+//     case 'es': imagePath = 'assets/images/qrCodes/AnaheimQRcodeES.svg'; break;
+//     case 'ru': imagePath = 'assets/images/qrCodes/AnaheimQRcodeRU.svg'; break;
+//     case 'ua': imagePath = 'assets/images/qrCodes/AnaheimQRcodeUA.svg'; break;
+//     default: imagePath = 'assets/images/qrCodes/AnaheimQRcodeEN.svg';
+//   }
+//   const imgElement = document.getElementById('language-image');   // TODO: image html tag
+//   imgElement.src = imagePath;
+// }
+    
--- js/lang.js
+++ js/lang.js
@@ -1,46 +1,148 @@
 const langArr = {
-  "unit": {
+  "title": {
     "en": "Life-saving ability!",
     "es": "¡Habilidad para salvar vidas!",
     "ru": "Навык спасения жизней!",
     "ua": "Навчки спасіння життів!",
-  }, 
-  "product-title": {
+  },
+  "subtitle": {
     "en": "School of nursing assistants",
     "es": "Escuela de auxiliares de enfermeria",
     "ru": "Школа помощников медсестёр",
     "ua": "Школа помічників медсестер",
-  }, 
-  "chip": {
+  },
+  "proposition": {
     "en": "For all those who have permission to work in the United States, are over 18 years of age and feel capable of passing the Department of Health exam in English, I offer you the opportunity to obtain the profession of nursing assistant",
     "es": "Para todos aquellos que tengan permiso para trabajar en los Estados Unidos, sean mayores de 18 años y se sientan capaces de aprobar el examen en inglés del Departamento de Salud, les ofrezco la oportunidad de obtener la profesión de asistente de enfermería",
     "ru": "всем, кто имеет разрешение на работу в США, кому исполнилось 18 лет и кто чувствует в себе силы сдать на английском экзамен в департаменте здравоохранения предлагаю получение профессии помощника медсестры",
     "ua": "всім, хто має дозвіл на роботу в США кому виповнилося 18 років і хто відчуває в собі сили здати іспит англійською у департаменті охорони здоровʼя пропоную отримання профессії помічника медичної сестри",
-  }, 
-  "memory": {
+  },
+  "think-twice-about-english": {
     "en": "After completing the course, you will need to pass an exam in English at the Department of Health within two weeks (the exam cannot be postponed, so think initially about your level of conversational English)",
     "es": "Una vez finalizado el curso, será necesario aprobar un examen en inglés en el Departamento de Salud en un plazo de dos semanas (no se puede posponer el examen, así que piense inicialmente en su nivel de inglés conversacional)",
     "ru": "по окончанию обучения необходимо будет в течение двух недель сдать экзамен в департаменте здравоохранения на английском языке (отсрочить экзамен не получится, поэтому изначально задумайтесь об уровне разговорного английского)",
     "ua": "по закінченню навчання необхідно буде впродовж двух наступних тижнів здати іспит у департаменті охорони здоровʼя англійською (відтермінувати здачу іспиту не можна, тому треба наперед критично оцінити власний рівень англійської)",
-  }, 
-  "memory-type": {
+  },
+  "how-many-days": {
     "en": "The course lasts 22 days, of which 14 days are theory and the rest practical",
     "es": "El curso tiene una duración de 22 días, de los cuales 14 días son teoría y el resto práctica",
     "ru": "обучение длится 22 дня, из них: 14 дней теория, остальное - практика",
     "ua": "термін навчання 22 дні, з них: 14 днів теорія, решта - практика",
-  }, 
-  "cool": {
+  },
+  "internship-saslary": {
     "en": "Attention! Practice pays! They will pay you for it;  for working during the internship in the hospital, you will receive a salary",
     "es": "¡Atención! ¡La práctica se paga! Le pagarán por ello; por trabajar durante la práctica en el hospital, recibirá un salario",
     "ru": "Внимание! Практика оплачивается! Вам за неё заплатят; за работу во время практики в госпитале Вы получите зарплату",
     "ua": "Увага! Практика оплачуватиметься! Вам за неї заплатять; за роботу під час практики в шпиталі Ви отримаєте зарплатню",
-  }, 
-  "more": {
+  },
+  "reimbursed": {
     "en": "That is, for your work in practice in the hospital, you will practically be reimbursed for the cost of the course",
     "es": "Es decir, por su trabajo en la práctica en el hospital, prácticamente le reembolsarán el costo del curso",
     "ru": "т.е. за Вашу работу на практике в госпитале Вам фактически вернут деньги за обучение",
     "ua": "тобто, за Вашу роботу на практиці в шпиталі Вам фактично повернуть гроші за навчання",
-  }, 
+  },
+  "course-cost": {
+    "en": "The cost of the course is $2000, plus $230 tax upon registration",
+    "es": "El costo del curso es de $2000, más $230 de impuestos al registrarse",
+    "ru": "Цена обучения: $2000 + $230 налог при регистрации",
+    "ua": "Ціна навчання: $2000 + $230 податок під час реєстрації;",
+  },
+  "split-payment-title": {
+    "en": "There are two options to pay for the course (not including taxes):",
+    "es": "Hay dos opciones para pagar el curso (sin incluir impuestos):",
+    "ru": "Есть два варианта разбивки оплаты за обучение(не включая налоги):",
+    "ua": "Маємо два способи розстрочки оплати за навчання (без податків):",
+  },
+  "split-payment-first-option": {
+    "en": "1.\&nbsp;You can divide the $2000 into 4 payments, but you must pay them all at the end of the theoretical part of the course, that is, on the 13th day from the start of the course",
+    "es": "1.\&nbsp;Puede dividir los $2000 en 4 pagos, pero debe pagarlos todos al finalizar la parte teórica del curso, es decir, el día 13 desde el inicio del curso",
+    "ru": "1.\&nbsp;$2000 можно разделить на 4 части, но выплатить их все надо к окончанию теоретической части обучения, т.е. дедлайн 13-й календарный день от начала обучения",
+    "ua": "1.\&nbsp;$2000 можна розділити на 4 частини, але виплатити їх всі треба до закінчення теоретичної частини навчання, тобто - дедлайн 13-й календарний день від початку навчання",
+  },
+  "split-payment-second-option": {
+    "en": "2.\&nbsp;You can split the $2000 into 2 payments, with the second being made after you are hired at the hospital where the internship will take place (i.e. this option is only valid for those who decide to work at that hospital for the first few years)",
+    "es": "2.\&nbsp;Puede dividir los $2000 en 2 pagos, y el segundo se realiza después de ser contratado en el hospital donde se llevará a cabo la práctica (es decir, esta opción solo es válida para aquellos que decidan trabajar en ese hospital durante los primeros años)",
+    "ru": "2.\&nbsp;$2000 можно разделить на 2 части, при этом вторую из них внести после трудоустройства в госпиталь, в котором будет проходить практика (т.е. этот способ подходит только для тех, кто решит первые годы работать в том госпитале, где будет проходить практика)",
+    "ua": "2.\&nbsp;$2000 можна розділити на 2 частини, при чому другу з них внести після працевлаштування у шпиталь, в якому буде проходити практика (тобто цей спосіб виключно для тих, хто вирішить перші роки працювати в тому шпиталі, де буде відпрацьовувати практику)",
+  },
+  "demanded-doc-title": {
+    "en": "You will have direct contact with the doctor who will conduct the training and prepare you for the exam at the Department of Health, as soon as you send me copies of your required documents:",
+    "es": "Tendrá contacto directo con el médico que llevará a cabo la formación y lo preparará para el examen en el Departamento de Salud, tan pronto como le envíe copias de sus documentos requeridos:",
+    "ru": "у Вас будет прямой и непосредственный контакт с доктором, который будет проводить обучение и готовить Вас к экзамену в управлении здравоохранения, как только я отправлю ему копии Ваших документов по перечню:",
+    "ua": "Ви матимете прямий і безпосередній звʼязок з доктором, которий буде проводити навчання і готувати Вас до іспиту в управлінні охорони здоровʼя,  з моменту, коли я відправлю йому копії Ваших документів за переліком:",
+  },
+  "demanded-doc-work-permit": {
+    "en": "1.\&nbsp;ID (+ Work permit)",
+    "es": "1.\&nbsp;ID (+ Work permit)",
+    "ru": "1.\&nbsp;ID (+ Work permit)",
+    "ua": "1.\&nbsp;ID (+ Work permit)",
+  },
+  "demanded-doc-ssn": {
+    "en": "2.\&nbsp;Social Security Number (last four of)",
+    "es": "2.\&nbsp;Social Security Number (last four of)",
+    "ru": "2.\&nbsp;Social Security Number (last four of)",
+    "ua": "2.\&nbsp;Social Security Number (last four of)",
+  },
+  "demanded-doc-covid-19": {
+    "en": "3.\&nbsp;COVID-19 vaccination",
+    "es": "3.\&nbsp;COVID-19 vaccination",
+    "ru": "3.\&nbsp;COVID-19 vaccination",
+    "ua": "3.\&nbsp;COVID-19 vaccination",
+  },
+  "demanded-doc-string-and": {
+    "en": "and",
+    "es": "también",
+    "ru": "и",
+    "ua": "та",
+  },
+  "demanded-doc-mantoux": {
+    "en": "4.\&nbsp;Tuberculosis test (Mantoux)",
+    "es": "4.\&nbsp;Tuberculosis test (Mantoux)",
+    "ru": "4.\&nbsp;Tuberculosis test (Mantoux)",
+    "ua": "4.\&nbsp;Tuberculosis test (Mantoux)",
+  },
+  "demanded-doc-additionally": {
+    "en": "Additionally, another document is required:",
+    "es": "Además, se requiere otro documento:",
+    "ru": "Eщё один документ:",
+    "ua": "Ще один документ:",
+  },
+  "demanded-doc-criminal-record": {
+    "en": "5.\&nbsp;Certificate of criminal record, at your discretion;",
+    "es": "5.\&nbsp;Certificado de antecedentes penales, según su criterio;",
+    "ru": "5.\&nbsp;Справка об отсутствии судимости",
+    "ua": "5.\&nbsp;Довідка про відсутність судимості",
+  },
+  "demanded-doc-criminal-record-disclamer": {
+    "en": "If you have it, you can bring it immediately, and if not, you can get it just before the course starts, since we have our own service for it",
+    "es": "si lo tiene, puede traerlo de inmediato, y si no, podrá obtenerlo justo antes de comenzar el curso, ya que tenemos nuestro propio servicio para ello",
+    "ru": "на Ваше усмотрение - если она есть, можете принести сразу, а если нет, можно будет получить её непосредственно перед началом обучения - у нас по соседству есть свой сервис",
+    "ua": "на Ваш розсуд - якщо Ви маєте Довідку, можете принести одразу, або можна буде отримати її безпосередньо перед початком навчання - ми маємо поблизу відповідний сервіс",
+  },
+  "contact-info-title": {
+    "en": "Contact information:",
+    "es": "información de contacto:",
+    "ru": "Контактная информация:",
+    "ua": "Контактна інформація:",
+  },
+  "contact-info-phone": {
+    "en": "+1 619 642 74 15",
+    "es": "+1 619 642 74 15",
+    "ru": "+1 619 642 74 15",
+    "ua": "+1 619 642 74 15",
+  },
+  "contact-info-whats-app": {
+    "en": "WhatsApp make connection faster!",
+    "es": "WhatsApp agiliza la conexión!",
+    "ru": "WhatsApp ускоряет коммуникацию!",
+    "ua": "WhatsApp прискорює коммунікацію!",
+  },
+  // "next": {
+  //   "en": "string",
+  //   "es": "string",
+  //   "ru": "string",
+  //   "ua": "string",
+  // },
 }
   
   
@@ -170,7 +272,7 @@ You will have direct contact with the doctor who will conduct the training
 and prepare you for the exam at the Department of Health,
 as soon as you send him copies of your required documents:
 1) Work permit
-2) Social Security Number (complete)
+2) Social Security Number (complete or last four of?)
 3) Vaccine against COVID-19
 and
 4) Tuberculosis test (Mantoux)
@@ -218,7 +320,7 @@ $2000 можна розділити на 4 частини, але виплати
 (тобто цей спосіб виключно для тих, хто вирішить перші роки працювати в тому шпиталі,
 де буде відпрацьовувати практику)
 
-у Вас буде прямий і безпосередній звʼязок з доктором, которий буде проводити навчання
+Ви матимете прямий і безпосередній звʼязок з доктором, которий буде проводити навчання
 і готувати Вас до іспиту в управлінні охорони здоровʼя,
 з моменту, коли я відправлю йому копії Ваших документів за переліком:
 1)ворк перміт;
@@ -231,3 +333,6 @@ $2000 можна розділити на 4 частини, але виплати
 5) довідка про відсутність судимості - на Ваш вибів - якщо вона є, можете принести одразу,
 а якщо ні, можна буде отримати її безпосередньо перед початком навчання - ми маємо відповідний сервіс
 */
+
+
+
